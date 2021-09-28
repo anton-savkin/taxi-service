44a41226fa2c4d6d5605d3285c4535a4c23c6505
anton-savkin <boxter2021@gmail.com>
2021-09-28 15:41:09 +0300

    Jenkins integration test

--- common/src/main/java/ru/digitalleague/common/TestClassFromCommon.java
+++ common/src/main/java/ru/digitalleague/common/TestClassFromCommon.java
@@ -4,4 +4,8 @@ public class TestClassFromCommon {
     public static void printHello() {
         System.out.println("Hello");
     }
+
+    public static void printJenkins() {
+        System.out.println("Jenkins");
+    }
 }
