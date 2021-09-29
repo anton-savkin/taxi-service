2e7d12873a39d457e1a75f9672bcf9b462ef3f53
anton-savkin <boxter2021@gmail.com>
2021-09-29 17:55:14 +0300

    Add core package structure

--- common/src/main/java/ru/digitalleague/common/TestClassFromCommon.java
+++
@@ -1,7 +0,0 @@
-package ru.digitalleague.common;
-
-public class TestClassFromCommon {
-    public static void printHello() {
-        System.out.println("Hello");
-    }
-}
--- core/src/main/java/ru/digitalleague/core/CoreApplication.java
+++ core/src/main/java/ru/digitalleague/core/CoreApplication.java
@@ -2,14 +2,12 @@ package ru.digitalleague.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.digitalleague.common.TestClassFromCommon;
 
 
 @SpringBootApplication
 public class CoreApplication {
 
     public static void main(String[] args) {
-        TestClassFromCommon.printHello();
         SpringApplication.run(CoreApplication.class, args);
     }
 
